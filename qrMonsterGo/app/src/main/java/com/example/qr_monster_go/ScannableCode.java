625b1c6f4fca06e3986090eac9b189c40c1ece03
MarcusWilson244 <mawilso1@ualberta.ca>
2023-03-09 16:02:43 -0700

    implemented name generation for ScannableCode

--- qrMonsterGo/app/src/main/java/com/example/qr_monster_go/ScannableCode.java
+++ qrMonsterGo/app/src/main/java/com/example/qr_monster_go/ScannableCode.java
@@ -7,14 +7,17 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ScannableCode {
-
     String code;
     int score;
+    String name;
 
     public ScannableCode(String code) {
         this.code = code;
         //calculate score in the constructor
         this.score = calculate_score(this.code);
+
+        this.name = generateName(this.code.substring(0, 2)); // only pass in first 2 characters(all that is needed for name generation)
+        Log.d(this.name, "ScannableCode: name");
     }
 
     public int calculate_score(String code){
@@ -52,6 +55,36 @@ public class ScannableCode {
         }
         Log.d(String.valueOf(repeats), "getRepeats: ");
         return repeats;
+    }
+
+    private String generateName(String code) {
+        ArrayList<String> names0 = new ArrayList<>(
+                Arrays.asList("cool", "Fro", "Mo", "Mega", "Spectral", "Crab"));
+        ArrayList<String> names1 = new ArrayList<>(
+                Arrays.asList("hot", "Glo", "Lo", "Ultra", "Sonic", "Shark"));
+
+        Log.d(code, "generateName: inputted string");
+        StringBuilder builder = new StringBuilder();
+
+        // convert code into binary
+        int hex = Integer.parseInt(code, 16);
+        String binary = Integer.toBinaryString(hex);
+
+        Log.d(binary, "generateName: binary representation");
+
+        for (int i = 0; i < 6; i++) {
+            if (binary.charAt(i) == '0') {
+                builder.append(names0.get(i));
+            } else {
+                builder.append(names1.get(i));
+            }
+
+            if (i == 0) {
+                builder.append(" ");
+            }
+        }
+
+        return builder.toString();
     }
 
     public String getCode() {
