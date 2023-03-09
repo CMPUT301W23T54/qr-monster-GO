832e7fa4c3bce4f2b0f0a7b7cff61bbac5eac422
MarcusWilson244 <mawilso1@ualberta.ca>
2023-03-08 18:57:35 -0700

    restructured code scanning activity/code to fix fragment display issue

---
+++ qrMonsterGo/app/src/main/java/com/example/qr_monster_go/ConfirmLocationDialog.java
@@ -0,0 +1,30 @@
+package com.example.qr_monster_go;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class ConfirmLocationDialog extends DialogFragment {
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        return new AlertDialog.Builder(requireContext())
+                .setTitle("Would you like to add your geo-location to this code?")
+                .setNegativeButton("No", null)
+                .setPositiveButton("Yes", (dialog, which) -> {
+                    ScanResultReceiver parent = (ScanResultReceiver) this.getActivity();
+                    parent.setLocationChoice();
+                })
+                .create();
+    }
+}
---
+++ qrMonsterGo/app/src/main/java/com/example/qr_monster_go/ScanResultReceiver.java
@@ -0,0 +1,6 @@
+package com.example.qr_monster_go;
+
+public interface ScanResultReceiver {
+    public void scanResultData(String codeFormat, String content);
+    public void setLocationChoice();
+}
