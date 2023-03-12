ade5ce76d13a5c8c0213d0ba6cce6293a73e966f
MarcusWilson244 <mawilso1@ualberta.ca>
2023-03-12 12:14:25 -0600

    fixed player stats bug

--- qrMonsterGo/app/src/main/java/com/example/qr_monster_go/HomePageActivity.java
+++ qrMonsterGo/app/src/main/java/com/example/qr_monster_go/HomePageActivity.java
@@ -3,6 +3,7 @@ package com.example.qr_monster_go;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -56,12 +57,13 @@ public class HomePageActivity extends AppCompatActivity {
         Leaderboards = findViewById(R.id.leaderboards);
         //Gets player stats and displays
         ArrayList<Integer> stats = new ArrayList<>();
-        stats = playerStats("Marcus");
-        high.setText("Highest Code " + stats.get(0));
-        low.setText("Lowest Code " + stats.get(1));
-        totalScore.setText("Total Score " + stats.get(2));
-        scannedCodes.setText("Scanned " + stats.get(3) + " codes");
         sharedPreferences = getSharedPreferences(sharedPreference, MODE_PRIVATE);
+        stats = playerStats(sharedPreferences.getString(key, ""));
+//        high.setText("Highest Code " + stats.get(0));
+//        low.setText("Lowest Code " + stats.get(1));
+//        totalScore.setText("Total Score " + stats.get(2));
+//        scannedCodes.setText("Scanned " + stats.get(3) + " codes");
+        //sharedPreferences = getSharedPreferences(sharedPreference, MODE_PRIVATE);
         username.setText(sharedPreferences.getString(key, null));
 
         ScanCodeButton.setOnClickListener(new View.OnClickListener() {
@@ -115,27 +117,61 @@ public class HomePageActivity extends AppCompatActivity {
                         Integer key =Integer.parseInt(document.get("codeScoreKey").toString()) ;
                         data.add(key);
                     }
+                    if(data.size() == 0){
+                        data.clear();
+                        data.add(0);
+                        data.add(0);
+                        data.add(0);
+                        data.add(0);
+                        //return data;
+                    }
+                    else {
+                        Collections.sort(data);
+                        int high = data.get(data.size() - 1);
+                        int low = data.get(0);
+
+                        int score = 0;
+                        for (int i = 0; i < data.size(); i++) {
+                            score += data.get(i);
+                        }
+
+                        int sum = data.size();
+                        data.clear();
+                        data.add(high);
+                        data.add(low);
+                        data.add(score);
+                        data.add(sum);
+                    }
+
+                    high.setText("Highest Code " + data.get(0));
+                    low.setText("Lowest Code " + data.get(1));
+                    totalScore.setText("Total Score " + data.get(2));
+                    scannedCodes.setText("Scanned " + data.get(3) + " codes");
+
+                    //return data;
+
                 }
             }
         });
-        if(data.size() == 0){
-            data.clear();
-            data.add(0);
-            data.add(0);
-            data.add(0);
-            data.add(0);
-            return data;
-        }
-        Collections.sort(data);
-        int high = data.get(data.size() - 1);
-        int low = data.get(0);
-        int score = 0;
-        int sum = data.size();
-        data.clear();
-        data.add(high);
-        data.add(low);
-        data.add(score);
-        data.add(sum);
+//        if(data.size() == 0){
+//            data.clear();
+//            data.add(0);
+//            data.add(0);
+//            data.add(0);
+//            data.add(0);
+//            return data;
+//        }
+//        Collections.sort(data);
+//        int high = data.get(data.size() - 1);
+//        int low = data.get(0);
+//        int score = 0;
+//        int sum = data.size();
+//        data.clear();
+//        data.add(high);
+//        data.add(low);
+//        data.add(score);
+//        data.add(sum);
+//        return data;
         return data;
     }
 }
