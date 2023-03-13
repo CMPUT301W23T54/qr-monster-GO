514dacaad72bc7e90329ab31dc9b29b44784f90a
Mu Du <mdu4@ualberta.ca>
2023-03-13 13:16:06 -0600

    finished intent testing

---
+++ qrMonsterGo/app/src/androidTest/java/com/example/qr_monster_go/ProfileActivityTest.java
@@ -0,0 +1,47 @@
+package com.example.qr_monster_go;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule ;
+import com.robotium.solo.Solo ;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class ProfileActivityTest {
+    private Solo solo;
+    @Rule
+    public ActivityTestRule<ProfileActivity> rule =
+            new ActivityTestRule<>(ProfileActivity.class, true, true);
+
+    /**
+     * Runs before all tests and creates solo instance.
+     *
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
+    }
+
+    /**
+     * Gets the Activity
+     *
+     * @throws Exception
+     */
+    @Test
+    public void start() throws Exception {
+        ProfileActivity activity = rule.getActivity();
+    }
+
+    /**
+     * Closes the activity after each test
+     *
+     * @throws Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+        solo.finishOpenedActivities();
+    }
+}
