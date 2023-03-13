89e8e293b3d816a3e0303f212ac50e45995cbc7a
AnshD8619 <ansh.desai86@gmail.com>
2023-03-12 22:45:00 -0600

    Can view and delete codes

--- qrMonsterGo/app/src/main/java/com/example/qr_monster_go/PlayerActivity.java
+++ qrMonsterGo/app/src/main/java/com/example/qr_monster_go/PlayerActivity.java
@@ -16,8 +16,12 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.model.Document;
+import com.google.firestore.v1.WriteResult;
 
 import java.util.ArrayList;
 
@@ -38,16 +42,15 @@ public class PlayerActivity extends AppCompatActivity {
         username = findViewById(R.id.username);
         returnButton = findViewById(R.id.home_return);
         deleteButton = findViewById(R.id.delete_button);
-        qrCodes = findViewById(R.id.users);
+        qrCodes = findViewById(R.id.codes);
         username.setText(getName);
         QrMonsterGoDB db = new QrMonsterGoDB();
+        data = new ArrayList<>();
         codesAdapter = new ArrayAdapter<>(this, R.layout.display, data);
-        qrCodes.setAdapter(codesAdapter);
-        data.clear();
         qrCodes.setAdapter(codesAdapter);
         CollectionReference usersReference = db.getCollectionReference("users");
         CollectionReference codesReference = db.getCollectionReference("CodeCollection");
-        codesReference.whereArrayContains("playerList", username).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+        codesReference.whereArrayContains("playerList", getName).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if(task.isSuccessful()){
@@ -68,6 +71,28 @@ public class PlayerActivity extends AppCompatActivity {
         deleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(location == -1){
+                    Toast.makeText(PlayerActivity.this, "Code not selected to remove", Toast.LENGTH_LONG).show();
+                }
+                else{
+                codesReference.whereEqualTo("name", data.get(location)).get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                String id = document.getId();
+                                ArrayList<String> update = new ArrayList<>();
+                                DocumentReference code = db.getDocumentReference(id, "CodeCollection");
+                                code.update("playerList", FieldValue.arrayRemove(getName));
+                                data.remove(location);
+                                location = -1;
+                            }
+                            qrCodes.setAdapter(codesAdapter);
+                        }
+                    }
+                });
+                }
             }
         });
         qrCodes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
