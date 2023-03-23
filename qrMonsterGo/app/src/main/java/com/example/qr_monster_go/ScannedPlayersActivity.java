04271dfb9bf9078108463af390ccaf9ed0e569ee
AnshD8619 <ansh.desai86@gmail.com>
2023-03-23 13:35:34 -0600

    Added comments funcionality

--- qrMonsterGo/app/src/main/java/com/example/qr_monster_go/ScannedPlayersActivity.java
+++ qrMonsterGo/app/src/main/java/com/example/qr_monster_go/ScannedPlayersActivity.java
@@ -8,13 +8,18 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
@@ -25,26 +30,37 @@ import java.util.List;
 
 public class ScannedPlayersActivity extends AppCompatActivity {
     ArrayList<String> data;
+    ArrayList<String> commentsData;
     ListView users;
+    ListView usersComments;
     ArrayAdapter<String> usersAdapter;
+    ArrayAdapter<String> commentsAdapter;
     TextView review;
     TextView visual;
     ImageButton returnButton;
+    EditText comments;
+    Button addComments;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanned_players);
         Intent intent = getIntent();
         String getName = intent.getStringExtra("code");
+        String uname = intent.getStringExtra("uname");
         QrMonsterGoDB db = new QrMonsterGoDB();
-        returnButton = returnButton.findViewById(R.id.return_button);
+        returnButton = findViewById(R.id.return_button);
+        usersComments = findViewById(R.id.userComments);
         users = findViewById(R.id.users);
         data = new ArrayList<>();
+        commentsData = new ArrayList<>();
         usersAdapter = new ArrayAdapter<>(this, R.layout.display, data);
+        commentsAdapter = new ArrayAdapter<>(this, R.layout.display, commentsData);
         users.setAdapter(usersAdapter);
-        review = review.findViewById(R.id.review);
+        review = findViewById(R.id.review);
         review.setText("Players who have scanned the same code");
-        visual = visual.findViewById(R.id.visual);
+        visual = findViewById(R.id.visual);
+        comments = findViewById(R.id.comments);
+        addComments = findViewById(R.id.addComments);
         CollectionReference codesReference = db.getCollectionReference("CodeCollection");
         codesReference.whereEqualTo("code", getName).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
@@ -52,10 +68,13 @@ public class ScannedPlayersActivity extends AppCompatActivity {
                 if(task.isSuccessful()) {
                     for (QueryDocumentSnapshot document : task.getResult()) {
                         String players = document.get("playerList").toString();
-
+                        String commentsArray = document.get("comments").toString();
                         ArrayList<String> person = new ArrayList<String>(Arrays.asList(players.replace("[", "").replace("]", "").split(", ")));
+                        ArrayList<String> review = new ArrayList<String>(Arrays.asList(commentsArray.replace("[", "").replace("]", "").split("//")));
                         data.addAll(person);
+                        commentsData.addAll(review);
                         users.setAdapter(usersAdapter);
+                        usersComments.setAdapter(commentsAdapter);
                     }
                     Log.d(String.valueOf(data), "onComplete: dataList");
                 }
@@ -67,7 +86,35 @@ public class ScannedPlayersActivity extends AppCompatActivity {
         returnButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(ScannedPlayersActivity.this, PlayerActivity.class));
+                Intent intent = new Intent(ScannedPlayersActivity.this, PlayerActivity.class);
+                intent.putExtra("username", uname);
+                startActivity(intent);
+            }
+        });
+        addComments.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String newComments = comments.getText().toString();
+                if(newComments.length() == 0){
+                    Toast.makeText(ScannedPlayersActivity.this, "No Comments", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    codesReference.whereEqualTo("code", getName).get()
+                            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                @Override
+                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                    if(task.isSuccessful()){
+                                        for (QueryDocumentSnapshot document : task.getResult()) {
+                                            String id = document.getId();
+                                            ArrayList<String> update = new ArrayList<>();
+                                            DocumentReference code = db.getDocumentReference(id, "CodeCollection");
+                                            code.update("comments", FieldValue.arrayUnion(newComments + "//"));
+                                            commentsAdapter.notifyDataSetChanged();
+                                        }
+                                    }
+                                }
+                            });
+                }
             }
         });
     }
