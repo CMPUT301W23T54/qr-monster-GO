976650b717749c45e0915f93db5a3b0074ed42bc
Mu Du <mdu4@ualberta.ca>
2023-04-03 12:57:33 -0600

    added PlayerActivity testing + added ranks in leaderboards

--- qrMonsterGo/app/src/main/java/com/example/qr_monster_go/home/PlayersAdapter.java
+++ qrMonsterGo/app/src/main/java/com/example/qr_monster_go/home/PlayersAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.qr_monster_go.R;
@@ -28,25 +29,47 @@ public class PlayersAdapter extends ArrayAdapter<Player> {
     }
     @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup
-            parent) {
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
 // return super.getView(position, convertView, parent);
         View view = convertView;
         if(view == null){
             view = LayoutInflater.from(context).inflate(R.layout.leaderboards_display, parent,false);
         }
+
         Player player = users.get(position);
         String score = player.getTotalScore().toString();
         String name = player.getUsername();
+        int rank = position + 1; // calculate the rank of the player
+
         if (name.length() < 20){
             while(name.length() < 20){
                 name += " ";
             }
         }
+
         TextView uName = view.findViewById(R.id.name);
         TextView scores = view.findViewById(R.id.scores);
+        TextView uRank = view.findViewById(R.id.rank);
+
         uName.setText("Username: " + name);
         scores.setText("Score: " + score);
+        uRank.setText("Rank: " + rank);
+
+//        uRank.setText(String.valueOf(rank));
+//
+//        // Set the icon for the top three players
+//        ImageView icon = view.findViewById(R.id.icon);
+//        if (rank == 1) {
+//            icon.setImageResource(R.drawable.gold_medal_icon);
+//        } else if (rank == 2) {
+//            icon.setImageResource(R.drawable.silver_medal_icon);
+//        } else if (rank == 3) {
+//            icon.setImageResource(R.drawable.bronze_medal_icon);
+//        } else {
+//            // Hide the icon for other players
+//            icon.setVisibility(View.GONE);
+//        }
+
         return view;
     }
 
