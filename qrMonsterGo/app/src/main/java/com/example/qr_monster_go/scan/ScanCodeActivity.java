cfee12c53a37a7e1f55af57cf915a10c9f9b3477
Norman <norman.x.zhang@gmail.com>
2023-04-02 22:12:22 -0600

    fixed re-scanning code after enabling permissions

--- qrMonsterGo/app/src/main/java/com/example/qr_monster_go/scan/ScanCodeActivity.java
+++ qrMonsterGo/app/src/main/java/com/example/qr_monster_go/scan/ScanCodeActivity.java
@@ -2,6 +2,7 @@ package com.example.qr_monster_go.scan;
 
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentSender;
@@ -325,6 +326,37 @@ public class ScanCodeActivity extends AppCompatActivity implements ScanResultRec
 
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                if (isGPSEnabled()) {
+
+                    setCurrentLocation();
+
+                } else {
+
+                    turnOnGPS();
+                }
+            }
+        }
+    }
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        if (requestCode == 2) {
+//            if (resultCode == Activity.RESULT_OK) {
+//
+//                setCurrentLocation();
+//            }
+//        }
+//    }
+
     /**
      * Function launches the image capturing activity
      * called through ConfirmImageDialog
@@ -349,6 +381,10 @@ public class ScanCodeActivity extends AppCompatActivity implements ScanResultRec
 
             scanResultData();
 
+        } else if (requestCode == 2) {
+            if (resultCode == Activity.RESULT_OK) {
+                setCurrentLocation();
+            }
         }
     }
 }
